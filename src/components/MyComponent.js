0b45b8e665d4bb3a116a2f47d450920ca3426db8
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-11-04 16:56:36 +0700

    "#19 DOM Events"

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,12 +6,21 @@ class MyComponent extends React.Component {
     address: 'Hoi dan it',
     age: 26,
   };
+  handleClick(event) {
+    console.log('>>>>> Click me my button!');
+    console.log(event.target);
+  }
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
+  }
   render() {
     return (
       <div>
         <h1>Hello, {this.state.name}</h1>
         <p>Address: {this.state.address}</p>
         <p>Age: {this.state.age}</p>
+        <button onClick={this.handleClick}>Click me!</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me!</button>
       </div>
     );
   }
