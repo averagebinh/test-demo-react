4f3f0019e1c1a011bc9a35a71e60cce82397634f
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-11-04 15:32:24 +0700

    "#16 state"

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+  state = {
+    name: 'Binh',
+    address: 'Hoi dan it',
+    age: 26,
+  };
   render() {
-    return <div>Hello world {Math.random()}</div>;
+    return (
+      <div>
+        <h1>Hello, {this.state.name}</h1>
+        <p>Address: {this.state.address}</p>
+        <p>Age: {this.state.age}</p>
+      </div>
+    );
   }
 }
 
