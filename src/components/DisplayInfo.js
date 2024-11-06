2cd0f3fd84a5ab44d0eb877f0d32a9bfcd73633f
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-11-06 10:35:01 +0700

    "#35 useState hook, update state"

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DisplayInfo.scss';
 import logo from './../logo.svg';
 //stateless vs stateful
@@ -39,9 +39,18 @@ import logo from './../logo.svg';
 //stateless
 const DisplayInfo = (props) => {
   const { listUsers } = props;
+  const [isShowHideListUser, setIsShowHideListUser] = useState(true);
+  const handleShowHideListUser = () => {
+    setIsShowHideListUser(!isShowHideListUser);
+  };
   return (
     <div className='display-info-container'>
-      {true && (
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser === true ? 'Hide List Users' : 'Show List Users'}
+        </span>
+      </div>
+      {isShowHideListUser && (
         <>
           {listUsers.map((user) => {
             return (
