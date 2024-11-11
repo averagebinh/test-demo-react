1d84626ac63d9fa38f46a1db04304c3b35cef71a
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-11-11 15:17:35 +0700

    "#67 implement Delete User"

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -93,6 +93,7 @@ const ManageUser = () => {
           show={showModalDeleteUser}
           setShow={setShowModalDeleteUser}
           dataDelete={dataDelete}
+          fetchListUsers={fetchListUsers}
         />
       </div>
     </div>
--- src/components/Admin/Content/ModalDeleteUser.js
+++ src/components/Admin/Content/ModalDeleteUser.js
@@ -1,13 +1,26 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import { deleteUser } from '../../../services/apiServices';
+import { toast } from 'react-toastify';
 const ModalDeleteUser = (props) => {
   const { show, setShow, dataDelete } = props;
 
   const handleClose = () => setShow(false);
-  const handleSubmitDeleteUser = () => {
+  const handleSubmitDeleteUser = async () => {
     alert('me');
+    let data = await deleteUser(dataDelete.id);
+    //EM err message
+    //EC err code 0 success 1 fail
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
+      handleClose();
+      await props.fetchListUsers();
+    }
+
+    if (data && data.EC !== 0) {
+      toast.error(data.EM);
+    }
   };
 
   return (
