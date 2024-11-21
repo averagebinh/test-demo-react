8b4170ba3cfa52974e8e9b0df5f6a53beb2fe1a2
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-11-21 12:18:21 +0700

    "#104 Design Timer count down"

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,14 +1,25 @@
+import CountDown from './CountDown';
+
 const RightContent = (props) => {
   const { dataQuiz } = props;
   console.log('dataQuiz ', dataQuiz);
+  const onTimeUp = () => {
+    props.handleFinishQuiz();
+  };
   return (
     <>
-      <div className='main-timer'>10:10</div>
+      <div className='main-timer'>
+        <CountDown onTimeUp={onTimeUp} />
+      </div>
       <div className='main-question'>
         {dataQuiz &&
           dataQuiz.length > 0 &&
           dataQuiz.map((data, index) => {
-            return <div className='question'>{index + 1} </div>;
+            return (
+              <div key={`question-abc-${index}`} className='question'>
+                {index + 1}{' '}
+              </div>
+            );
           })}
       </div>
     </>
--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -165,7 +165,7 @@ const DetailQuiz = (props) => {
         </div>
       </div>
       <div className='right-content'>
-        <RightContent dataQuiz={dataQuiz} />
+        <RightContent dataQuiz={dataQuiz} handleFinishQuiz={handleFinishQuiz} />
       </div>
       <ModalResult
         show={isShowResult}
